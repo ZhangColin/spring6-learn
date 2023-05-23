6ae19d860bce7c0fc7a8263f11322670553f4b55
colin <zhang_colin@163.com>
2023-05-23 14:16:16 +0800

    add log4j2

--- spring-first/src/main/java/com/example/TestUser.java
+++ spring-first/src/main/java/com/example/TestUser.java
@@ -1,12 +1,16 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.lang.reflect.InvocationTargetException;
 
 public class TestUser {
+    private Logger logger = LoggerFactory.getLogger(TestUser.class);
+
     @Test
     public void testUserObject(){
         // 加载 Spring 配置文件，对象创建
@@ -18,6 +22,8 @@ public class TestUser {
 
         // 使用对象调用方法进行测试
         user.add();
+
+        logger.info("执行调用成功了。。。");
     }
 
     @Test
