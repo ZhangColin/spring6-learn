47940b74d2e86b417998ee04875a566d13a70cf9
colin <zhang_colin@163.com>
2023-05-23 20:10:11 +0800

    通过接口类型获取实现类

---
+++ spring6-ioc-xml/src/main/java/com/example/bean/TestUserDao.java
@@ -0,0 +1,14 @@
+package com.example.bean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestUserDao {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+
+        UserDao dao = (UserDao)context.getBean(UserDao.class);
+        System.out.println(dao);
+        dao.run();
+    }
+}
---
+++ spring6-ioc-xml/src/main/java/com/example/bean/UserDaoImpl.java
@@ -0,0 +1,8 @@
+package com.example.bean;
+
+public class UserDaoImpl implements UserDao{
+    @Override
+    public void run() {
+        System.out.println("run ...");
+    }
+}
