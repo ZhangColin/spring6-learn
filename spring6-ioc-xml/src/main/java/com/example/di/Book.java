6d5e737b3985d6d9e11376706a8fc5a3d6bc8476
colin <zhang_colin@163.com>
2023-05-23 20:54:13 +0800

    Setter注入与构造注入

---
+++ spring6-ioc-xml/src/main/java/com/example/di/Book.java
@@ -0,0 +1,38 @@
+package com.example.di;
+
+public class Book {
+    public Book() {
+    }
+
+    public Book(String bookName, String author) {
+        this.bookName = bookName;
+        this.author = author;
+    }
+
+    private String bookName;
+    private String author;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
